3125fa3c349d9b5207ad15420fa0cb2b4bef8d7d
Hai Ho <haiho@Hais-MacBook-Pro.local>
2022-11-08 15:38:02 +1100

    check flake8

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,12 @@
 import json
 import dotenv
 
+
 # MQTT client callback functions
 def on_publish(client, userdata, mid):
     """Callback when message is published"""
     print(" [INFO] Published to " + str(mid))
+
 
 def on_connect(client, userdata, flags, rc):
     """Callback when client connect to broker"""
@@ -13,14 +15,17 @@ def on_connect(client, userdata, flags, rc):
     else:
         print(" [ERROR] Connection refused")
 
+
 def on_subscribe(client, userdata, mid, granted_qos):
     """Callback when client subscribed to a topic"""
     print(" [INFO] Subscribed to " + str(mid) + " " + str(granted_qos))
-    
+
+
 def on_message(client, userdata, message):
     """Callback when client received message from a topic"""
     msg = json.loads(message.payload.decode('utf-8'))
     print(" [INFO] Received %s from topic %s" % (msg, message.topic))
+
 
 # Configurations
 def load_system_config(cfg_file):
@@ -28,6 +33,7 @@ def load_system_config(cfg_file):
     with open(cfg_file, 'r') as f:
         cfg = json.load(f)
     return cfg
+
 
 def load_env_config(dotenv_file):
     """Load environment secret configuration file"""
